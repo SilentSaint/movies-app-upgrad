b5caf2c33b4c76ff657c5ad9d9d9dbbf497aebb0
SilentSaint <rakshithg35@gmail.com>
2021-06-04 12:28:06 +0530

    checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Header.css";
+import logo from "./../../assets/logo.svg";
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <img src={logo} className="logo" alt="movies-app-logo" />
+      </div>
+    );
+  }
+}
+
+export default Header;
