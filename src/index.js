5632fa424e803f0e13d2d8dbb7df67614b61f4bc
SilentSaint <rakshithg35@gmail.com>
2021-06-06 21:31:41 +0530

    checkpoint 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Header from "./common/header/Header";
+import Home from "./screens/home/Home";
 ReactDOM.render(
   <div>
-    <Header />
+    <Home />
   </div>,
   document.getElementById("root")
 );
